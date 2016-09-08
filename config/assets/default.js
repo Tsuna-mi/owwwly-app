cdbbc1d933131fcd26e9f0f0fb94eca275e879fb
Ana Gamito <imaginarte3d@gmail.com>
2016-09-08 23:39:33 +0200

    changes on config app

--- config/assets/default.js
+++ config/assets/default.js
@@ -9,7 +9,7 @@ module.exports = {
         // bower:css
         'public/lib/bootstrap/dist/css/bootstrap.css',
         'public/lib/bootstrap/dist/css/bootstrap-theme.css',
-        'public/lib/ng-img-crop/compile/unminified/ng-img-crop.css'
+        'public/lib/ng-img-crop/compile/minified/ng-img-crop.css',
         // endbower
       ],
       js: [
@@ -17,13 +17,14 @@ module.exports = {
         'public/lib/angular/angular.js',
         'public/lib/angular-animate/angular-animate.js',
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
-        'public/lib/ng-file-upload/ng-file-upload.js',
-        'public/lib/ng-img-crop/compile/unminified/ng-img-crop.js',
         'public/lib/angular-messages/angular-messages.js',
         'public/lib/angular-mocks/angular-mocks.js',
         'public/lib/angular-resource/angular-resource.js',
         'public/lib/angular-ui-router/release/angular-ui-router.js',
+        'public/lib/ng-file-upload/ng-file-upload.js',
+        'public/lib/ng-img-crop/compile/minified/ng-img-crop.js',
         'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
+        'public/lib/bootstrap-sass/assets/javascripts/bootstrap.js',
         // endbower
       ],
       tests: ['public/lib/angular-mocks/angular-mocks.js']
