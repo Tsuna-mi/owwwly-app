1b3cf29812a5a19396d1626220aeb3681cdba534
Ana Gamito <imaginarte3d@gmail.com>
2016-09-09 01:41:42 +0200

    restore files to init config before heroku

--- gruntfile.js
+++ gruntfile.js
@@ -325,8 +325,7 @@ module.exports = function (grunt) {
   });
 
   // Lint project files and minify them into two production files.
-  grunt.registerTask('build', ['env:dev', 'wiredep', 'lint', 'ngAnnotate']);
-  // grunt.registerTask('build', ['env:dev', 'wiredep', 'lint', 'ngAnnotate', 'uglify', 'cssmin']);
+  grunt.registerTask('build', ['env:dev', 'wiredep', 'lint', 'ngAnnotate', 'uglify', 'cssmin']);
 
   // Run the project tests
   grunt.registerTask('test', ['env:test', 'lint', 'mkdir:upload', 'copy:localConfig', 'server', 'mochaTest', 'karma:unit', 'protractor']);
