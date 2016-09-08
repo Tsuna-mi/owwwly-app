560eb6d0f90aaacee1779211247a9a5fd7d40240
Ana Gamito <imaginarte3d@gmail.com>
2016-09-08 13:26:13 +0200

    Fix permissions to delete/update boards

--- modules/boards/server/policies/boards.server.policy.js
+++ modules/boards/server/policies/boards.server.policy.js
@@ -25,10 +25,10 @@ exports.invokeRolesPolicies = function () {
     roles: ['user'],
     allows: [{
       resources: '/api/boards',
-      permissions: ['get', 'post']
+      permissions: '*'
     }, {
       resources: '/api/boards/:boardId',
-      permissions: ['get']
+      permissions: '*'
     }]
   }, {
     roles: ['guest'],
